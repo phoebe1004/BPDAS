bd1522356acec944a4911781d2e5299ac26f99ec
Phoebe Claire Lanquino <89074978+phoebe1004@users.noreply.github.com>
2022-11-21 13:56:06 +0800

    Added bootstrap Scaffolded admin panel

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.base')
+
+@section('body')
+    <div class='main d-flex'>
+        <div class="sidebar"></div>
+        <div class="content flex-fill"></div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
+
+Route::get('/admin', [AdminController::class, 'home'])->name('admin-home');
